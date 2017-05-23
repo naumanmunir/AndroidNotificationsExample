f41bf2a32555bbfdb56fe1180c90bb84489f8063
nonabona <bloodo9@hotmail.com>
2017-05-22 22:22:12 -0400

    first

--- AndroidNotificationsExample/MainActivity.cs
+++ AndroidNotificationsExample/MainActivity.cs
@@ -1,18 +1,38 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace AndroidNotificationsExample
 {
     [Activity(Label = "AndroidNotificationsExample", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+
+        private static readonly int btnClickNotification = 9999;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            var btnSend = FindViewById<Button>(Resource.Id.btnSend);
+
+            btnSend.Click += BtnSend_Click;
+        }
+
+        private void BtnSend_Click(object sender, System.EventArgs e)
+        {
+            Bundle valuesSend = new Bundle();
+            valuesSend.PutString("sendContent", "This is content sent from activity 1");
+
+            Intent newIntent = new Intent(this, typeof(Second_Activity));
+
+            newIntent.PutExtras(valuesSend);
+
+            Android.Support.V4.App.TaskStackBuilder stackBuilder = new Android.Support.V4.App.TaskStackBuilder.
         }
     }
 }
--- AndroidNotificationsExample/Resources/Resource.Designer.cs
+++ AndroidNotificationsExample/Resources/Resource.Designer.cs
@@ -57,11 +57,33 @@ namespace AndroidNotificationsExample
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int btnSend = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Second_Activity = 2130903041;
 			
 			static Layout()
 			{
